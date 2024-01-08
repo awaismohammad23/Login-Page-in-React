fc0566b11c4e64d5152f27c7ed1cb07f739d254d
Awais Mohammad <awaismadad@gmail.com>
2024-01-08 22:39:08 +0500

    login page completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { LoginSignup } from './components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div>
+      
+      <LoginSignup />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './LoginSignup.css'
+import { useState } from 'react'
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+
+
+export const LoginSignup = () => {
+
+    const [action, setAction] = useState("Login");
+
+  return (
+    <div className='container'>
+        <div className='header'>
+            <div className='text'>{action}</div>
+            <div className='underline'></div>
+
+            </div>
+
+            <div className='inputs'>
+                {action==="Login"?<div></div>: <div className='input'>
+                
+                <img src={user_icon} alt='user icon' />
+                <input type='text' placeholder='Username' />
+                </div>
+            }
+           
+
+            <div className='input'>
+                <img src={email_icon} alt='user icon' />
+                <input type='email' placeholder='Email' />
+            </div>
+
+            <div className='input'>
+                <img src={password_icon} alt='user icon' />
+                <input type='password' placeholder='Password' />
+            </div> 
+        </div>
+
+        <div className="submit-container">
+            <div className={action==="Login"? "submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"? "submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+        </div>
+        </div>
+  )
+}
